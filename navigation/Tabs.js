b68de935560994fce6ca706d32e1f0f296472d5e
SunEom <dkxl0701@gmail.com>
2021-01-08 14:58:32 +0900

    Update stack header inside tab navigation

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,14 +4,23 @@ import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
 import Favs from '../screens/Favs';
+import { useLayoutEffect } from 'react';
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-  <Tabs.Navigator>
-    <Tabs.Screen name="Movies" component={Movies} />
-    <Tabs.Screen name="Tv" component={Tv} />
-    <Tabs.Screen name="Search" component={Search} />
-    <Tabs.Screen name="Favs" component={Favs} />
-  </Tabs.Navigator>
-);
+export default ({ navigation, route }) => {
+  const getHeaderName = (route) => route?.state?.routeNames[route.state.index];
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: getHeaderName(route) || 'Movies' });
+  }, [route]);
+
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Movies" component={Movies} />
+      <Tabs.Screen name="Tv" component={Tv} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favourites" component={Favs} />
+    </Tabs.Navigator>
+  );
+};
