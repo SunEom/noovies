33b13f6e74d9ec13d5603943d72b02c286f936cf
SunEom <dkxl0701@gmail.com>
2021-01-14 16:50:52 +0900

    create airing today part of Tv

--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import styled from 'styled-components/native';
+import Horizontal from '../../components/Horizontal';
 import HorizontalSlider from '../../components/HorizontalSlider';
+import List from '../../components/List';
 import ScrollContainer from '../../components/ScorllContainer';
 import Vertical from '../../components/Vertical';
 
-export default ({ loading, popular, topRated }) => (
+const Container = styled.View`
+  margin-top: 30px;
+`;
+
+export default ({ loading, popular, topRated, today }) => (
   <ScrollContainer loading={loading}>
-    <HorizontalSlider title={'Popular Shows'}>
-      {popular.map((show) => (
-        <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
+    <Container>
+      <HorizontalSlider title={'Popular Shows'}>
+        {popular.map((show) => (
+          <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
+        ))}
+      </HorizontalSlider>
+      <HorizontalSlider title={'Top Rated'}>
+        {topRated.map((show) => (
+          <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
+        ))}
+      </HorizontalSlider>
+    </Container>
+    <List title={'Airing Today'}>
+      {today.map((show) => (
+        <Horizontal key={show.id} id={show.id} poster={show.poster_path} title={show.name} overview={show.overview} />
       ))}
-    </HorizontalSlider>
-    <HorizontalSlider title={'Top Rated'}>
-      {topRated.map((show) => (
-        <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
-      ))}
-    </HorizontalSlider>
+    </List>
   </ScrollContainer>
 );
