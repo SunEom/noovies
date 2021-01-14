2bff449e3c2c90e54724aadfcbe8d86c8f9e6b76
SunEom <dkxl0701@gmail.com>
2021-01-14 17:03:58 +0900

    change popular UI HorizontalSlider to SwiperSlider

--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -3,7 +3,9 @@ import styled from 'styled-components/native';
 import Horizontal from '../../components/Horizontal';
 import HorizontalSlider from '../../components/HorizontalSlider';
 import List from '../../components/List';
+import Slide from '../../components/Movies/Slide';
 import ScrollContainer from '../../components/ScorllContainer';
+import SwiperSlider from '../../components/SwiperSlider';
 import Vertical from '../../components/Vertical';
 
 const Container = styled.View`
@@ -12,12 +14,21 @@ const Container = styled.View`
 
 export default ({ loading, popular, topRated, today }) => (
   <ScrollContainer loading={loading}>
+    <SwiperSlider>
+      {popular.map((show) => (
+        <Slide
+          key={show.id}
+          id={show.id}
+          title={show.name}
+          overview={show.overview}
+          votes={show.vote_average}
+          backgroundImage={show.backdrop_path}
+          poster={show.poster_path}
+        />
+      ))}
+    </SwiperSlider>
+
     <Container>
-      <HorizontalSlider title={'Popular Shows'}>
-        {popular.map((show) => (
-          <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
-        ))}
-      </HorizontalSlider>
       <HorizontalSlider title={'Top Rated'}>
         {topRated.map((show) => (
           <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
