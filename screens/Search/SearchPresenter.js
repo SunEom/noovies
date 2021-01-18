b8edbe7adc146306527a01f2e5e51753696ae0e3
SunEom <dkxl0701@gmail.com>
2021-01-18 16:54:42 +0900

    fix props on search result

--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -15,14 +15,14 @@ export default ({ movies, shows, onChange, keyword, onSubmit }) => (
     {movies.length !== 0 && (
       <HorizontalSlider title={'Movie results'}>
         {movies.map((movie) => (
-          <Vertical key={movie.id} id={movie.id} poster={movie.poster_path} title={movie.title} overview={movie.overview} />
+          <Vertical key={movie.id} id={movie.id} poster={movie.poster_path} title={movie.title} votes={movie.vote_average} />
         ))}
       </HorizontalSlider>
     )}
     {shows.length !== 0 && (
       <HorizontalSlider title={'TV results'}>
         {shows.map((show) => (
-          <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} overview={show.overview} />
+          <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} votes={show.vote_average} />
         ))}
       </HorizontalSlider>
     )}
