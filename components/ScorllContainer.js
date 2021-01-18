09c827e3cc23c45c9c8b086d00b94862893f6a36
SunEom <dkxl0701@gmail.com>
2021-01-18 17:24:54 +0900

    create onRefresh on ScrollContainer

--- components/ScorllContainer.js
+++ components/ScorllContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ActivityIndicator, Dimensions, RefreshControl, ScrollView, View } from 'react-native';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
+import { useState } from 'react/cjs/react.development';
 
 const Container = styled.View`
   flex: 1;
@@ -9,19 +10,27 @@ const Container = styled.View`
 
 const HEIGHT = Dimensions.get('screen').height / 2;
 
-const ScrollContainer = ({ loading, children }) => (
-  <Container>
-    <ScrollView
-      refreshControl={<RefreshControl tintColor={'white'} />}
-      style={{ backgroundColor: 'black' }}
-      contentContainerStyle={{
-        justifyContent: loading ? 'center' : 'flex-start',
-      }}
-    >
-      {loading ? <ActivityIndicator style={{ height: HEIGHT }} color="white" size="small" /> : children}
-    </ScrollView>
-  </Container>
-);
+const ScrollContainer = ({ refreshFn, loading, children }) => {
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refreshFn();
+    setRefreshing(false);
+  };
+  return (
+    <Container>
+      <ScrollView
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={'white'} />}
+        style={{ backgroundColor: 'black' }}
+        contentContainerStyle={{
+          justifyContent: loading ? 'center' : 'flex-start',
+        }}
+      >
+        {loading ? <ActivityIndicator style={{ height: HEIGHT }} color="white" size="small" /> : children}
+      </ScrollView>
+    </Container>
+  );
+};
 
 ScrollContainer.propTypes = {
   loading: PropTypes.bool.isRequired,
@@ -29,3 +38,9 @@ ScrollContainer.propTypes = {
 };
 
 export default ScrollContainer;
+
+const onRefresh = async () => {
+  setRefreshing(true);
+  await getData();
+  setRefreshing(false);
+};
