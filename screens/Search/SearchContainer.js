a5f8f21bb1eeb2e318deeb69f56a5dd9ae909b2a
SunEom <dkxl0701@gmail.com>
2021-01-18 16:43:33 +0900

    get search result

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,4 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+import { movieApi, tvApi } from '../../API';
 import SearchPresenter from './SearchPresenter';
 
-export default () => <SearchPresenter />;
+export default () => {
+  const [keyword, setKeyword] = useState('');
+  const [results, setResults] = useState({
+    movies: [],
+    shows: [],
+    moviesError: null,
+    showsError: null,
+  });
+  const onChange = (text) => setKeyword(text);
+  const search = async () => {
+    const [movies, moviesError] = await movieApi.search(keyword);
+    const [shows, showsError] = await tvApi.search(keyword);
+    setResults({
+      movies,
+      shows,
+      moviesError,
+      showsError,
+    });
+  };
+
+  return <SearchPresenter {...results} onChange={onChange} onSubmit={search} keyword={keyword} />;
+};
--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import Horizontal from '../../components/Horizontal';
+import HorizontalSlider from '../../components/HorizontalSlider';
 import Input from '../../components/Search/Input';
+import Vertical from '../../components/Vertical';
 
 const Container = styled.ScrollView`
   background-color: black;
 `;
 
-export default () => (
+const Text = styled.Text``;
+export default ({ movies, shows, onChange, keyword, onSubmit }) => (
   <Container>
-    <Input placeholder={'Write a keyword'} />
+    <Input placeholder={'Write a keyword'} onChange={onChange} value={keyword} onSubmit={onSubmit}></Input>
+    <HorizontalSlider title={'Movie results'}>
+      {movies.map((movie) => (
+        <Vertical key={movie.id} id={movie.id} poster={movie.poster_path} title={movie.title} overview={movie.overview} />
+      ))}
+    </HorizontalSlider>
+    <HorizontalSlider title={'TV results'}>
+      {shows.map((show) => (
+        <Vertical key={show.id} id={show.id} poster={show.poster_path} title={show.name} overview={show.overview} />
+      ))}
+    </HorizontalSlider>
   </Container>
 );
