ee57c2ecd5c4e405cdcdbe3c2513de6c9713c389
SunEom <dkxl0701@gmail.com>
2021-01-20 10:17:40 +0900

    downgrade image quality

--- API.js
+++ API.js
@@ -42,5 +42,5 @@ export const tvApi = {
 
 export const apiImage = (path) =>
   path
-    ? `https://image.tmdb.org/t/p/original${path}`
+    ? `https://image.tmdb.org/t/p/w500/${path}`
     : 'https://images.unsplash.com/photo-1545164630-64ccc5195afb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80';
