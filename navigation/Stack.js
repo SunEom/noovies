4f6789c18c07922064135c27643df2c701d39867
SunEom <dkxl0701@gmail.com>
2021-01-20 11:44:52 +0900

    fix a source of Detail component

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Detail from '../screens/Detail';
+import Detail from '../screens/Detail/index';
 import Tabs from './Tabs';
 
 const Stack = createStackNavigator();
