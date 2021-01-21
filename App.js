8a1961c706d41b3425dda6f3071daefa81d9180c
SunEom <dkxl0701@gmail.com>
2021-01-21 17:41:13 +0900

    preload FontAwesome

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { Image, StatusBar, View } from 'react-native';
 import { Asset } from 'expo-asset';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import Stack from './navigation/Stack';
 
@@ -26,7 +26,7 @@ export default function App() {
       'https://images.unsplash.com/photo-1606851682541-2c947e1ddc9b?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       require('./assets/splash.png'),
     ]);
-    const fonts = cacheFonts([Ionicons.font]);
+    const fonts = cacheFonts([Ionicons.font, FontAwesome.font]);
     return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
